1afeac122d4c0015a455cbb8792cb66870095087
bmhclt <bmhclt@users.noreply.github.com>
2022-09-20 13:57:30 +0530

    Prayer Room

---
+++ prayer-room/data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-b-block---ground-floor---entrance",
+      "name": "B Block - Ground Floor - Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008878538644605527,
+          "pitch": 0.15174954189879486,
+          "rotation": 0,
+          "target": "1-b-block---ground-floor---reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-b-block---ground-floor---reception",
+      "name": "B Block - Ground Floor - Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.806550921271187,
+        "pitch": 0.21224321812853297,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4803193296212616,
+          "pitch": 0.08180686886955613,
+          "rotation": 0,
+          "target": "2-b-block---3rd-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-b-block---3rd-floor",
+      "name": "B Block - 3rd Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.8242354806955348,
+        "pitch": 0.24030782766247683,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12209144442399023,
+          "pitch": 0.09934741211026932,
+          "rotation": 12.566370614359176,
+          "target": "3-b-block---3rd-floor---way-to-a-block"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-b-block---3rd-floor---way-to-a-block",
+      "name": "B Block - 3rd Floor - Way to A Block",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004441130578690888,
+          "pitch": 0.008881990935059036,
+          "rotation": 1.5707963267948966,
+          "target": "4-b-block--3rd-floor---prayer-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-b-block--3rd-floor---prayer-room",
+      "name": "B Block- 3rd Floor - Prayer Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5408790591833057,
+        "pitch": 0.15290612985628016,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
