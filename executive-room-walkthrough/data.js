70da6269794e0583bdaa50da166e74e8f4a1d097
bmhclt <bmhclt@users.noreply.github.com>
2022-09-20 10:43:48 +0530

    Executive room walkthrough

---
+++ executive-room-walkthrough/data.js
@@ -0,0 +1,150 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-executive-ac-b-block---1",
+      "name": "Executive AC B Block - 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.9459567660503847,
+        "pitch": 0.20733283212571862,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3304333016917766,
+          "pitch": 0.1144894904641518,
+          "rotation": 0,
+          "target": "1-executive-ac-b-block---2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5162752102628163,
+          "pitch": -0.07070290969875437,
+          "title": "Television",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.9414780527800248,
+          "pitch": 0.11229384482701121,
+          "title": "Air Conditioner Control",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.3400147581514243,
+          "pitch": 0.14497271092323416,
+          "title": "Nurse Calling Switch",
+          "text": "<a href='https://www.youtube.com/watch?v=gGfQPfXXSKA&ab_channel=BabyMemorialHospital' target='_blank'>📹 Medication Safety</a>"
+        }
+      ]
+    },
+    {
+      "id": "1-executive-ac-b-block---2",
+      "name": "Executive AC B Block - 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5987821895303647,
+        "pitch": 0.10292684912612415,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.637227436128672,
+          "pitch": 0.20945420500340362,
+          "rotation": 0,
+          "target": "0-executive-ac-b-block---1"
+        },
+        {
+          "yaw": 1.6410969859822435,
+          "pitch": 0.2442865320355878,
+          "rotation": 0,
+          "target": "2-executive-ac-b-block---bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-executive-ac-b-block---bathroom",
+      "name": "Executive AC B Block - Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.06847060911670155,
+        "pitch": 0.020217773935485184,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9931596935748637,
+          "pitch": 0.14216680668627824,
+          "rotation": 0,
+          "target": "1-executive-ac-b-block---2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
