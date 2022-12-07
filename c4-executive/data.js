10254f1e76ae7fdc9a1c0c471372a3bd07965be8
bmhclt <bmhclt@users.noreply.github.com>
2022-12-07 10:46:39 +0530

    C4 Executive Room

---
+++ c4-executive/data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-c4-ac-executive",
+      "name": "C4 AC executive",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.7478614338221234,
+        "pitch": 0.21434551879654506,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.5494753853206538,
+          "pitch": -0.18847372424703224,
+          "title": "Television",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/channels/directory/' target='_blank'>View TV Directory</a>"
+        },
+        {
+          "yaw": 1.9591604788931392,
+          "pitch": 0.48712592609995653,
+          "title": "Dining",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/dining/' target='_blank'>View Dining Facilities</a>"
+        },
+        {
+          "yaw": -2.910150078221065,
+          "pitch": 0.06758409967039469,
+          "title": "Telephone",
+          "text": "<a href='https://adm.bmhcare.in/nav/directory/' target='_blank'>View Telephone Directory</a>"
+        },
+        {
+          "yaw": -2.7496675844947163,
+          "pitch": 0.2392086059447749,
+          "title": "Doctors",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/doctors/' target='_blank'>View Doctors List</a>"
+        }
+      ]
+    }
+  ],
+  "name": "C4 Executive Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
