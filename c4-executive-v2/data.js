13ecca1d4ba6ba23866de8df68450eee4a27f79a
bmhclt <bmhclt@users.noreply.github.com>
2022-12-08 16:44:12 +0530

    New version with icon links and assets folder

---
+++ c4-executive-v2/data.js
@@ -0,0 +1,84 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-c4-ac-executive--1",
+      "name": "C4 Ward - Executive Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3729876554930325,
+          "pitch": -0.20937791872466605,
+          "title": "TV",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/channels/directory/' target='_blank'><button type='button' class='btn btn-primary'><i class='fas fa-external-link-alt'></i> View TV Directory</button></a>",          
+          "icon": "tv",
+        },
+        {
+          "yaw-orig": 1.8059448342843591,
+          "yaw": 1.75,
+          "pitch-orig": 0.5387676144787878,
+          "pitch": 0.45,
+          "title": "Dietary",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/dining/' target='_blank'><button type='button' class='btn btn-primary'><i class='fas fa-external-link-alt'></i> View Dietary Facilities</button></a>",          
+          "icon": "food",
+        },
+        {
+          "yaw": -3.0377135962450854,
+          "yaw": -3.1,
+          "pitch": 0.08127499444717401,
+          "pitch": 0.03,
+          "title": "Telephone",
+          "text": "<a href='https://adm.bmhcare.in/nav/directory/' target='_blank'><button type='button' class='btn btn-primary'><i class='fas fa-external-link-alt'></i> View Telephone Directory</button></a>",          
+          "icon": "telephone",
+        },
+        {
+          "yaw-orig": -2.901435792353233,
+          "yaw": -2.95,
+          "pitch-orig": 0.3014319575377158,
+          "pitch": 0.21,
+          "title": "Doctors",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/doctors/' target='_blank'><button type='button' class='btn btn-primary'><i class='fas fa-external-link-alt'></i> View Doctors List</button></a>",          
+          "icon": "doctor",
+        },
+        {
+          "yaw": -2.35,
+          "pitch": 0.15,
+          "title": "Info Portal",
+          "text": "<a href='https://adm.bmhcare.in/info_portal/' target='_blank'><button type='button' class='btn btn-primary'><i class='fas fa-external-link-alt'></i> Open Information Portal</button></a>",          
+          "icon": "book",
+        }
+      ]
+    }
+  ],
+  "name": "C4 Ward - Executive Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
